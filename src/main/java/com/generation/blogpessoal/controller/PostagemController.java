4e03211066bcf245c063c81398108f96d65adc48
milabizzle <parcerias.cr@gmail.com>
2023-06-27 19:38:34 -0300

    CRUD parte 2

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -1,18 +1,22 @@
 package com.generation.blogpessoal.controller;
 
 import java.util.List;
-
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
@@ -27,27 +31,46 @@ public class PostagemController {
 	@Autowired
 	private PostagemRepository postagemRepository;
 
-	@GetMapping
+	@GetMapping //lista todas as postagens
 	public ResponseEntity<List<Postagem>> getAll(){
 		return ResponseEntity.ok(postagemRepository.findAll());
 	}
 	
-	@GetMapping("/{id}")
+	@GetMapping("/{id}") //Lista uma postagem por id
 	public ResponseEntity<Postagem> getById(@PathVariable Long id){
 		return postagemRepository.findById(id)
 				.map(resposta -> ResponseEntity.ok(resposta))
 				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
 	}
 	
-	@GetMapping("/titulo/{titulo}")
+	@GetMapping("/titulo/{titulo}") //Lista as postagens pelo título
 	public ResponseEntity <List<Postagem>> getByTitulo(@PathVariable String titulo){
 		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
 	}
 	
-	@PostMapping
+	@PostMapping //Cadastro de postagem
 	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem){
 		return ResponseEntity.status(HttpStatus.CREATED)
 				.body(postagemRepository.save(postagem));
+	
+	}	
 		
+		@PutMapping
+		public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem) {
+			return postagemRepository.findById(postagem.getId())
+					.map(resposta -> ResponseEntity.status(HttpStatus.OK)
+					.body(postagemRepository.save(postagem)))
+					.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+			
 	}
+		@ResponseStatus(HttpStatus.NO_CONTENT)
+		@DeleteMapping("/{id}")
+		public void delete(@PathVariable Long id) {
+			Optional<Postagem> postagem = postagemRepository.findById(id);
+			
+			if(postagem.isEmpty())
+				throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+			
+			postagemRepository.deleteById(id);
+		}
 }

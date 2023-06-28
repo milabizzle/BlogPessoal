35510a2bf48847b3ab7a2e45b0ccb33f57300566
milabizzle <parcerias.cr@gmail.com>
2023-06-28 16:15:33 -0300

    CRUD parte 3

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -20,6 +20,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
+import com.generation.blogpessoal.repository.TemaRepository;
 
 import jakarta.validation.Valid;
 
@@ -30,6 +31,9 @@ public class PostagemController {
 	
 	@Autowired
 	private PostagemRepository postagemRepository;
+	
+	@Autowired
+	private TemaRepository temaRepository;
 
 	@GetMapping //lista todas as postagens
 	public ResponseEntity<List<Postagem>> getAll(){
@@ -50,17 +54,26 @@ public class PostagemController {
 	
 	@PostMapping //Cadastro de postagem
 	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem){
+		if (temaRepository.existsById(postagem.getTema().getId()))
 		return ResponseEntity.status(HttpStatus.CREATED)
 				.body(postagemRepository.save(postagem));
 	
+		throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Tema não existe!", null);
 	}	
 		
 		@PutMapping
-		public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem) {
-			return postagemRepository.findById(postagem.getId())
-					.map(resposta -> ResponseEntity.status(HttpStatus.OK)
-					.body(postagemRepository.save(postagem)))
-					.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+		public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem){
+			if (postagemRepository.existsById(postagem.getTema().getId())) {
+				
+				if (temaRepository.existsById(postagem.getTema().getId()))
+					return ResponseEntity.status(HttpStatus.OK)
+							.body(postagemRepository.save(postagem));
+			
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Tema não existe!", null);
+			
+			}
+			
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 			
 	}
 		@ResponseStatus(HttpStatus.NO_CONTENT)
